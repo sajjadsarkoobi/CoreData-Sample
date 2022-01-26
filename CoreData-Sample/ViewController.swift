e844acb638191619c172adb92a3a05ed0bcade02
ssarkoobi@planatechnologies.com <sajjadsarkoobi@gmail.com>
2022-01-26 11:15:58 +0300

    Initial Commit

---
+++ CoreData-Sample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoreData-Sample
+//
+//  Created by Sajjad Sarkoobi on 1/26/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
